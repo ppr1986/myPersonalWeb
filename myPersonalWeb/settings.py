3a0c689f757f7002c428fdc086f9e107db9cd576
Pedro Enrique <ppry1986@gmail.com>
2022-06-18 18:27:36 -0400

    adding static folder

--- myPersonalWeb/settings.py
+++ myPersonalWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,6 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
